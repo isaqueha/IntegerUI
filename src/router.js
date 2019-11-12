1425ed8669c75e858e78f0d00aee8d6016ad0b75
Isaque Azevedo <isaque.azevedo@sap.com>
2019-11-11 23:34:05 -0300

    delete unused files

--- src/router.js
+++ src/router.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-import New from "./pages/New";
 
 export default function Router() {
 	return (
@@ -11,7 +10,6 @@ export default function Router() {
 			<Switch>
 				<Route path="/" exact component={Login} />
 				<Route path="/dashboard" component={Dashboard} />
-				<Route path="/new" component={New} />
 			</Switch>
 		</BrowserRouter>
 	)
