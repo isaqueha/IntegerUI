f6b05d1bb36544e4b9582ca0c2338a514d0ccd47
Isaque Azevedo <isaque.azevedo@sap.com>
2019-11-12 01:34:25 -0300

    Add Google OAuth

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import api from '../../services/api';
 import Cookies from 'js-cookie';
+import GoogleLogin from 'react-google-login';
 
 export default function Login({ history }) {
 	const [email, setEmail] = useState('');
@@ -8,7 +9,25 @@ export default function Login({ history }) {
   
   async function handleSubmit(event) {
 		event.preventDefault();
+		// TODO Refactor duplicate code
     const response = await api.post('/user', { email, password });
+
+		const { api_key } = response.data;
+		Cookies.set('api_key', api_key, { expires: 1 });
+		
+		history.push('/dashboard');
+	}
+
+	async function onSignIn(googleUser) {
+		if (!googleUser.profileObj.googleId || !googleUser.profileObj.email) {
+			return false;
+		}
+
+		console.log(googleUser.profileObj);
+		const response = await api.post('/user', { 
+			email: googleUser.profileObj.email, 
+			password: googleUser.profileObj.googleId
+		});
 
 		const { api_key } = response.data;
 		Cookies.set('api_key', api_key, { expires: 1 });
@@ -21,6 +40,14 @@ export default function Login({ history }) {
 			<p>
 				Create your User to get <strong>Integer Incrementing features</strong>.
 			</p>
+			<GoogleLogin
+				className="googleSign"
+				clientId="770435751242-d0ufvib7h68tgsdphcmv26dd12erpmie.apps.googleusercontent.com"
+				buttonText="Log in"
+				onSuccess={onSignIn}
+				onFailure={onSignIn}
+				cookiePolicy={'single_host_origin'}
+			/>
 
 			<form onSubmit={handleSubmit}>
 				<label htmlFor="email">E-Mail *</label>
