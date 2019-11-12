ad9ce6115a6869892734415e30565c3bba8f2c81
Isaque Azevedo <isaque.azevedo@sap.com>
2019-11-11 23:59:38 -0300

    fix name and service mapping

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+let connect = {
+	baseURL: "https://integerservice.herokuapp.com/"
+}
+
 const api = axios.create({
-	baseURL: 'http://localhost:3333',
+	baseURL: connect.baseURL,
 });
 
 export default api;
